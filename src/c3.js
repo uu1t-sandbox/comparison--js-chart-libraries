1d8878be912bf6de8d1cd30454793a253a8fac88
Yuichi Tanikawa <u1.tanikawa@gmail.com>
2018-12-27 15:25:19 +0900

    Add C3.js example

---
+++ src/c3.js
@@ -0,0 +1,38 @@
+import 'c3/c3.min.css'
+import c3 from 'c3'
+
+document.addEventListener('DOMContentLoaded', () => {
+  c3.generate({
+    bindto: '#chart',
+    data: {
+      x: 'x',
+      columns: [
+        ['x', '2015-07-01', '2016-07-01', '2017-07-01', '2018-07-01'],
+        ['人数1', 16, 15, 15, 17],
+        ['人数2', 100, 120, 135, 150],
+        ['割合1', 80, 82, 85, 90],
+        ['割合2', 20, 18, 15.3, 12.5]
+      ],
+      axes: {
+        割合1: 'y2',
+        割合2: 'y2'
+      },
+      types: {
+        人数1: 'bar',
+        人数2: 'bar'
+      },
+      groups: [['人数1', '人数2']]
+    },
+    axis: {
+      x: {
+        type: 'timeseries',
+        tick: {
+          format: '%Y/%m'
+        }
+      },
+      y2: {
+        show: true
+      }
+    }
+  })
+})
